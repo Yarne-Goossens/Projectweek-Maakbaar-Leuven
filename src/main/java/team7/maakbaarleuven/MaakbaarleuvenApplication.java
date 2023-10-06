e37312e059f17af9d5d36f3b1e086c745431f372
BriniodeGroot <91272260+BriniodeGroot@users.noreply.github.com>
2023-10-06 14:21:17 +0200

    initial setup v2

---
+++ src/main/java/team7/maakbaarleuven/MaakbaarleuvenApplication.java
@@ -0,0 +1,13 @@
+package team7.maakbaarleuven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaakbaarleuvenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaakbaarleuvenApplication.class, args);
+	}
+
+}
