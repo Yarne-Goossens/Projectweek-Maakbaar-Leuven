774976a94452a1b62c54db47006a412f0015e2e0
Karel <r0892111@ucll.be>
2023-10-13 13:11:40 +0200

    eerste twee buttons werken

--- src/main/resources/js/repair.js
+++ src/main/resources/js/repair.js
@@ -1,13 +1,43 @@
 
-const displayDiv = () => {
+const displayMainDiv = () => {
     const div = document.getElementById('maindiv');
     createDropDown();
     createNextButton();
     document.querySelector('button').addEventListener('click', () => {
         const value = document.querySelector('select').value;
         console.log(value);
+        div.innerHTML = ""
+        enterModel();
+
+    
     });
 
+}
+
+const enterModel = () =>{
+    const div = document.getElementById('maindiv')
+    const label1 = document.createElement('label')
+    label1.innerHTML = "req1"
+    const input1 = document.createElement('input')
+    input1.innerHTML = "req1"
+    const label2 = document.createElement('label')
+    label2.innerHTML = "req1"
+    const input2 = document.createElement('input')
+    input2.innerHTML = "req1"
+    const label3 = document.createElement('label')
+    label3.innerHTML = "req1"
+    const input3 = document.createElement('input')
+    input3.innerHTML = "req1"
+    div.appendChild(label1)
+    div.appendChild(input1)
+    div.appendChild(label2)
+    div.appendChild(input2)
+    div.appendChild(label3)
+    div.appendChild(input3)
+    const button = document.createElement('button')
+    button.innerHTML = "Start"
+    div.appendChild(button)
+    
 }
 
 const createDropDown = () => {
@@ -31,8 +61,10 @@ const createDropDown = () => {
 const createNextButton = () => {
     const button = document.createElement('button');
     button.innerHTML = 'Volgende';
+    button.id = "repair_form"
+    
     const div = document.getElementById('maindiv');
     div.appendChild(button);
 }
 
-displayDiv();
+displayMainDiv();
