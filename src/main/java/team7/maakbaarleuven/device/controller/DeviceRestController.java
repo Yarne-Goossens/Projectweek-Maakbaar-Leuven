10dd68dd9165e8c168f8229fb5a6eed96502af95
QuentinGillisUCLL <quentin.gillis@student.ucll.be>
2023-10-13 10:03:29 +0200

    Begin repo

--- src/main/java/team7/maakbaarleuven/device/controller/DeviceRestController.java
+++ src/main/java/team7/maakbaarleuven/device/controller/DeviceRestController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import team7.maakbaarleuven.device.model.Device;
 import team7.maakbaarleuven.device.service.DeviceService;
 
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "http://localhost:5500")
 @RestController
 @RequestMapping("/api")
 public class DeviceRestController {
