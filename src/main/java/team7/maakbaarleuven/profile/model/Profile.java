78fb76d7a14256c9f9b6ebc728ea19221a9c7b52
Jules Defloor <jules.defloor@student.ucll.be>
2023-10-20 13:36:47 +0200

    unused imports in comments gezet

--- src/main/java/team7/maakbaarleuven/profile/model/Profile.java
+++ src/main/java/team7/maakbaarleuven/profile/model/Profile.java
@@ -8,6 +8,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "profiles")
 public class Profile {
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Id
--- src/main/java/team7/maakbaarleuven/profile/repo/ProfileRepository.java
+++ src/main/java/team7/maakbaarleuven/profile/repo/ProfileRepository.java
@@ -1,8 +1,8 @@
 package team7.maakbaarleuven.profile.repo;
 
-import java.util.List;
+//import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+//import org.springframework.stereotype.Repository;
 
 import team7.maakbaarleuven.profile.model.Profile;
 
