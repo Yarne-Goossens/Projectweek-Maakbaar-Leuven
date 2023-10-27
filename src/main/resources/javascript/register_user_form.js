2944f85eb69fcd71a4628c036e210ca3a622e51b
Yarne-Goossens <yarne.goossens@student.ucll.be>
2023-10-27 11:54:58 +0200

    regex for for first/lastname and email

--- src/main/resources/javascript/register_user_form.js
+++ src/main/resources/javascript/register_user_form.js
@@ -2,22 +2,44 @@ addStatusError = (status) => {
     document.getElementById("statusError").innerHTML = "<p class='error'>" + status + "</p>";
   };
 
+  const clearStatus = (status) =>
+  (document.getElementById("statusError").innerHTML = "");
+
 const registerUser = async () => {
     const firstname = document.getElementById('Firstname').value;
     const lastname = document.getElementById('Lastname').value;
     const email = document.getElementById('user-email').value;
     const password = document.getElementById('user-password').value;
 
-    console.log(email);
-
-
-    const regex=new RegExp("^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$");
-    console.log(regex.test(email))
-    if (!regex.test(email)) {
+    const regex_email=new RegExp("^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$");
+    if (!regex_email.test(email)) {
         console.log("Yes");
-        throw new addStatusError("Email not valid");
+        addStatusError("Email is niet geldig");
         return;
     }
+
+    const regex_firstname=new RegExp("[A-Za-z]+-?[A-Za-z]+$");
+    console.log(regex_firstname.test(firstname))
+    if (!regex_firstname.test(firstname)) {
+        console.log("Yes");
+        addStatusError("Voornaam is niet geldig");
+        return;
+    }
+
+    const regex_lastname=new RegExp("[A-Za-z]+-?[A-Za-z]+$");
+    console.log(regex_lastname.test(lastname))
+    if (!regex_lastname.test(lastname)) {
+        console.log("Yes");
+        addStatusError("Achternaam is niet geldig");
+        return;
+    }
+
+    if(password.length < 8) {
+        addStatusError("Wachtwoord moet minimaal 8 karakters bevatten");
+        return;
+    }
+    
+    clearStatus();
 
     const profile = {
         firstname,
