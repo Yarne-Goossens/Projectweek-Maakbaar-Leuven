c97cd62b6df3f33c3513b7d155eb3f28006b7b31
Jules Defloor <jules.defloor@student.ucll.be>
2023-10-27 11:00:58 +0200

    niet werkende versie

--- src/main/java/team7/maakbaarleuven/WebConfig.java
+++ src/main/java/team7/maakbaarleuven/WebConfig.java
@@ -1,16 +1,22 @@
 package team7.maakbaarleuven;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebMvc
-public class WebConfig implements WebMvcConfigurer {
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedMethods("GET", "POST");
+public class WebConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://127.0.0.1:5500")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedHeaders("*");
+            }
+        };
     }
 }
--- src/main/resources/js1/login.js
+++ src/main/resources/js1/login.js
@@ -7,35 +7,45 @@ const addStatus = (status) => {
 };
 const main = async () => {
     console.log("hier");
-    const login_button = document.getElementById('login_form');
+    const login_button = document.getElementById('submit-btn');
     login_button.addEventListener('click', async () => {
         const password_input = document.getElementById('user-password').value;
         console.log(`passw_input: ${password_input}`);
         const email_input = document.getElementById('user-email').value;
         console.log(`email_input: ${email_input}`);
         const profile = { email: email_input, password: password_input };
+        console.log(profile);
         console.log("voor auth");
         const status = await authenticate(profile);
+        console.log("status:" + status);
         console.log("na auth")
         addStatus(status);
     });
 }
 
 const authenticate = async (profile) => {
-    const response = await fetch(`http://localhost:8080/api/profile/authenticate`, {
-        method: 'POST',
-        headers: {
-            Accept: 'application/json',
-            'Content-Type': 'application/json',
-        }, body: JSON.stringify(profile),
-    });
-    const result = await response.json();
-    if (response.status === 400) {
-        console.log("Device is not added.");
-    } else {
-        console.log("werkt wel");
+    try {
+        const response = await fetch(`http://localhost:8080/api/profile/authenticate`, {
+            method: 'POST',
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(profile),
+        });
+
+        if (!response.ok) {
+            throw new Error(`Failed to fetch: ${response.status} - ${response.statusText}`);
+        }
+
+        const result = await response.json();
+        return result;
+    } catch (error) {
+        // Handle the error here, log it, or return an appropriate error message.
+        console.error("Error in fetch request:", error);
+        throw error; // Optionally, re-throw the error for further handling.
     }
-    return result;
-}
+};
+
 
 main();
