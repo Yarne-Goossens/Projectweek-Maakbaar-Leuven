8a64fabde68cb2577d5aeee75e8c127f2414731a
Jules Defloor <jules.defloor@student.ucll.be>
2023-11-10 13:40:41 +0100

    temp

--- src/main/resources/js/login.js
+++ src/main/resources/js/login.js
@@ -12,17 +12,21 @@ const main = async () => {
         const password_input = document.getElementById('user-password').value;
         const email_input = document.getElementById('user-email').value;
         const profile = {
-            email: email_input, 
+            email: email_input,
             password: password_input,
         };
         const status = await authenticate(profile);
         console.log(status);
-        if(status) {
+        if (status) {
             addStatus("login gelukt");
+            const user = await getUser(profile.email);
+            console.log(user)
+            sessionStorage.setItem('user', user.firstname);
+            window.location.href = "http://127.0.0.1:5500/wpp-2324-team-07/src/main/resources/templates/user.html";
         } else {
             addStatus("login mislukt");
         }
-        
+
     });
 }
 
@@ -51,5 +55,47 @@ const authenticate = async (profile) => {
     }
 };
 
+const getUser = async (email) => {
+    try {
+        const response = await fetch(`http://127.0.0.1:8080/api/profile/${email}`, {
+            method: 'GET',
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json'
+            },
+        });
+        //console.log(response);
+
+        // if (!response.ok) {
+        //     throw new Error(`Failed to fetch: ${response.status} - ${response.statusText}`);
+        // }
+
+        const result = await response.json();
+        return result;
+    } catch (error) {
+        // Handle the error here, log it, or return an appropriate error message.
+        console.error("Error in fetch request:", error);
+        throw error; // Optionally, re-throw the error for further handling.
+    }
+};
+
+checkUserLogin = () => {
+    if (sessionStorage.getItem('user')) {
+        const loginButton = document.getElementById('login');
+        loginButton.remove();
+
+        var navigationList = document.getElementById("navigation_list");
+        var newListItem = document.createElement("li");
+        var newLink = document.createElement("a");
+        newLink.href = "user.html";
+        newLink.textContent = "Profile";
+
+        newListItem.appendChild(newLink);
+        navigationList.appendChild(newListItem);
+    }
+
+};
+
 
 main();
+window.addEventListener('load', checkUserLogin());
