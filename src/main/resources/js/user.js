5b829aa15dcbd6e3d54ec62d86cb2aefcb7e9c7b
YannickVandereycken <91282328+YannickVandereycken@users.noreply.github.com>
2023-11-24 14:41:45 +0100

    user script on booking page

--- src/main/resources/js/user.js
+++ src/main/resources/js/user.js
@@ -1,49 +1,46 @@
 checkUserLogin = () => {
-    if (sessionStorage.getItem('user')) {
-        console.log('hier2');
-        const loginButton = document.getElementById('login');
-        if (loginButton) {
-            loginButton.innerHTML = "Logout";
-            loginButton.id = "logout"
-        }
+	if (sessionStorage.getItem("user")) {
+		console.log("hier2");
+		const loginButton = document.getElementById("login");
+		if (loginButton) {
+			loginButton.innerHTML = "Logout";
+			loginButton.id = "logout";
+		}
 
-        const logoutButton = document.getElementById('logout');
-        if (logoutButton) {
-            logoutButton.addEventListener('click', async (event) => {
-                sessionStorage.clear();
-                logoutButton.innerHTML = "Login";
-                logoutButton.id = 'login';
-                window.location.href = "login.html"
+		const logoutButton = document.getElementById("logout");
+		if (logoutButton) {
+			logoutButton.addEventListener("click", async (event) => {
+				sessionStorage.clear();
+				logoutButton.innerHTML = "Login";
+				logoutButton.id = "login";
+				window.location.href = "login.html";
+			});
+		}
+	}
+	if (sessionStorage.getItem("role") === "USER") {
+		var navigationList = document.getElementById("navigation_list");
+		if (navigationList) {
+			var newListItem = document.createElement("li");
+			var newLink = document.createElement("a");
+			newLink.href = "user.html";
+			newLink.textContent = "Profile";
 
-            })
-        }
-    }
-    if (sessionStorage.getItem('role') === 'USER') {
-        var navigationList = document.getElementById("navigation_list");
-        if (navigationList) {
-            var newListItem = document.createElement("li");
-            var newLink = document.createElement("a");
-            newLink.href = "user.html";
-            newLink.textContent = "Profile";
+			newListItem.appendChild(newLink);
+			navigationList.appendChild(newListItem);
+		}
+	}
+	if (sessionStorage.getItem("role") === "ADMIN") {
+		var navigationList = document.getElementById("navigation_list");
+		if (navigationList) {
+			var newListItem = document.createElement("li");
+			var newLink = document.createElement("a");
+			newLink.href = "admin.html";
+			newLink.textContent = "Admin";
 
-            newListItem.appendChild(newLink);
-            navigationList.appendChild(newListItem);
-        }
-    }
-    if (sessionStorage.getItem('role') === 'ADMIN') {
-        var navigationList = document.getElementById("navigation_list");
-        if (navigationList) {
-            var newListItem = document.createElement("li");
-            var newLink = document.createElement("a");
-            newLink.href = "admin.html";
-            newLink.textContent = "Admin";
-
-            newListItem.appendChild(newLink);
-            navigationList.appendChild(newListItem);
-        }
-    }
-
+			newListItem.appendChild(newLink);
+			navigationList.appendChild(newListItem);
+		}
+	}
 };
 
-
-window.addEventListener('load', checkUserLogin);
+window.addEventListener("load", checkUserLogin);
