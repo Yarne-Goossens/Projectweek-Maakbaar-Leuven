35b88ee5b1741373a1ce9bddb3b66ef0f4e62ab0
Jules Defloor <jules.defloor@student.ucll.be>
2023-11-24 11:30:41 +0100

    werkend

--- src/main/resources/js/user.js
+++ src/main/resources/js/user.js
@@ -33,37 +33,4 @@ checkUserLogin = () => {
 
 };
 
-const getUser = async (email) => {
-    const response = await fetch(`http://localhost:8080/api/profile/${email}`, {
-        method: "GET",
-        headers: {
-            Accept: "application/json",
-            "Content-Type": "application/json",
-        },
-    });
-    const result = await response.json();
-    console.log(result);
-    return result;
-};
-
-
-const displayUserInfo = async () => {
-    const user = await getUser(sessionStorage.getItem('email'));
-    const userdiv = document.getElementById('userInfo');
-
-    const card = document.createElement('div');
-    card.id = 'userCard';
-
-    const cardContent = `
-        <h2>${user.firstname} ${user.lastname}</h2>
-        <p>Email: ${user.email}</p>
-        <p>Role: ${user.role}</p>
-    `;
-
-    card.innerHTML = cardContent;
-
-    userdiv.appendChild(card);
-}
-
-displayUserInfo();
 window.addEventListener('load', checkUserLogin);
